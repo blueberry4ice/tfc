01ce5cfa94f2246c3d1bdb1b203dc755c6e5dc7b
Lauren <laurensius.sakti@atibusinessgroup.com>
2022-08-24 19:19:12 +0700

    lazyload meneh

--- resources/views/livewire/footer-component.blade.php
+++ resources/views/livewire/footer-component.blade.php
@@ -17,90 +17,90 @@
                 <div class="partner-list">
                     <div class="bold fz12 partner-name">Bank Partner</div>
                     <div class="partner-logo">
-                        <a href="https://www.bca.co.id/en/Individu" target="_blank"><img src="{{ asset('assets/img/5-bank.png') }}" data-src="{{ asset('assets/img/5-bank.png') }}" alt="" style="max-width: 75%" class="lazy"> </a>
+                        <a href="https://www.bca.co.id/en/Individu" target="_blank"><img data-src="{{ asset('assets/img/5-bank.png') }}" alt="" style="max-width: 75%" class="lazy"> </a>
                     </div>
                 </div>
                 <div class="partner-list">
                     <div class="bold fz12 partner-name">Insurance Partner</div>
                     <div class="partner-logo">
-                        <a href="https://www.bcainsurance.co.id/" target="_blank" style="height: 78px; display: flex; align-items: center"><img src="{{ asset('assets/img/logo bcai-01.png') }}" data-src="{{ asset('assets/img/logo bcai-01.png') }}" alt="" class="lazy"></a>
+                        <a href="https://www.bcainsurance.co.id/" target="_blank" style="height: 78px; display: flex; align-items: center"><img data-src="{{ asset('assets/img/logo bcai-01.png') }}" alt="" class="lazy"></a>
                     </div>
                 </div>
                 <div class="partner-list">
                     <div class="bold fz12 partner-name">Telecommunication Partner</div>
                     <div class="partner-logo">
-                        <a href="https://www.telkomsel.com/" target="_blank"><img src="{{ asset('assets/img/telkomsel.png') }}" data-src="{{ asset('assets/img/telkomsel.png') }}" alt="" style="max-width: 80%" class="lazy"></a>
+                        <a href="https://www.telkomsel.com/" target="_blank"><img data-src="{{ asset('assets/img/telkomsel.png') }}" alt="" style="max-width: 80%" class="lazy"></a>
                     </div>
                 </div>
                 <div class="partner-list partner-group partner-tech">
                     <div class="bold fz12 partner-name">Technology Partner</div>
                     <div class="partner-logo">
                         <a target="_blank" class="img-nto" href="http://www.travelportindonesia.com" style="width: 33.3334%">
-                            <img src="{{ asset('assets/img/galileo.png') }}" data-src="{{ asset('assets/img/galileo.png') }}" alt="" class="lazy" style="padding: 0;">
+                            <img data-src="{{ asset('assets/img/galileo.png') }}" alt="" class="lazy" style="padding: 0;">
                         </a>
                         <a target="_blank" class="img-nto" href="https://atibusinessgroup.com/" style="width: 33.3334%">
-                            <img src="{{ asset('assets/img/logo-ati.png') }}" data-src="{{ asset('assets/img/logo-ati.png') }}" class="lazy" style="width: 50%" alt="">
+                            <img data-src="{{ asset('assets/img/logo-ati.png') }}" class="lazy" style="width: 50%" alt="">
                         </a>
                     </div>
                 </div>
                 <div class="partner-list partner-group">
                     <div class="bold fz12 partner-name">Participating Airlines</div>
                     <div class="partner-logo" style="display: flex;">
-                        <img src="{{ asset('assets/img/airlines/ana.png') }}" data-src="{{ asset('assets/img/airlines/ana.png') }}" alt="" class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/cathaypacific_master-logo_vertical_green_en.png') }}" data-src="{{ asset('assets/img/airlines/cathaypacific_master-logo_vertical_green_en.png') }}"
+                        <img data-src="{{ asset('assets/img/airlines/ana.png') }}" alt="" class="ic_sq lazy">
+                        <img data-src="{{ asset('assets/img/airlines/cathaypacific_master-logo_vertical_green_en.png') }}"
                             alt="" class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/Logo type Citilink-01 hijau.png') }}" data-src="{{ asset('assets/img/airlines/Logo type Citilink-01 hijau.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/Logo type Citilink-01 hijau.png') }}" alt=""
                             class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/01 Intl-Sig-SkyTeam-4C.jpg') }}" data-src="{{ asset('assets/img/airlines/01 Intl-Sig-SkyTeam-4C.jpg') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/01 Intl-Sig-SkyTeam-4C.jpg') }}" alt=""
                             class="ic_sq" class="lazy" style="max-width: 40%">
-                        <img src="{{ asset('assets/img/airlines/ek.png') }}" data-src="{{ asset('assets/img/airlines/ek.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/ek.png') }}" alt=""
                             class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/ey.png') }}" data-src="{{ asset('assets/img/airlines/ey.png') }}" alt="" class="ic_sq lazy">
+                        <img data-src="{{ asset('assets/img/airlines/ey.png') }}" alt="" class="ic_sq lazy">
                     </div>
                     <div class="partner-logo" style="display: flex;">
-                        <img src="{{ asset('assets/img/airlines/EVA STAR LOGO-1-2.1v-01.jpg') }}" data-src="{{ asset('assets/img/airlines/EVA STAR LOGO-1-2.1v-01.jpg') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/EVA STAR LOGO-1-2.1v-01.jpg') }}" alt=""
                             class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/garuda.png') }}" data-src="{{ asset('assets/img/airlines/garuda.png') }}" alt="" 
+                        <img data-src="{{ asset('assets/img/airlines/garuda.png') }}" alt="" 
                             class="ic_sq lazy" style="max-width: 35%">
-                        <img src="{{ asset('assets/img/airlines/Japan Airlines Logo White_Square.png') }}" data-src="{{ asset('assets/img/airlines/Japan Airlines Logo White_Square.png') }}" alt="" 
+                        <img data-src="{{ asset('assets/img/airlines/Japan Airlines Logo White_Square.png') }}" alt="" 
                             class="ic_sq lazy" style="max-width: 20%">
-                        <img src="{{ asset('assets/img/airlines/MH_Logo.png') }}" data-src="{{ asset('assets/img/airlines/MH_Logo.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/MH_Logo.png') }}" alt=""
                             class="ic_sq lazy" style="width: 35%">
-                        <img src="{{ asset('assets/img/airlines/PAS_Logo_Final_220107.png') }}" data-src="{{ asset('assets/img/airlines/PAS_Logo_Final_220107.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/PAS_Logo_Final_220107.png') }}" alt=""
                             class="ic_sq lazy" style="width: 35%">
-                        <img src="{{ asset('assets/img/airlines/4-Star PAL logo 2-liner W-01.png') }}" data-src="{{ asset('assets/img/airlines/4-Star PAL logo 2-liner W-01.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/4-Star PAL logo 2-liner W-01.png') }}" alt=""
                             class="ic_sq lazy" style="width: 35%">
                     </div>
                     <div class="partner-logo" style="display: flex;">
-                        <img src="{{ asset('assets/img/airlines/QANTAS_SOA_Masterbrand_Horizontal_RGB_190816.jpg') }}" data-src="{{ asset('assets/img/airlines/QANTAS_SOA_Masterbrand_Horizontal_RGB_190816.jpg') }}" 
+                        <img data-src="{{ asset('assets/img/airlines/QANTAS_SOA_Masterbrand_Horizontal_RGB_190816.jpg') }}" 
                         alt="" class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/qr.png') }}" data-src="{{ asset('assets/img/airlines/qr.png') }}" alt="" class="ic_qr lazy" style="max-width: 30%">
-                        <img src="{{ asset('assets/img/airlines/sq.png') }}" data-src="{{ asset('assets/img/airlines/sq.png') }}" alt="" class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/LOGO SRIWIJAYA AIR HORIZONTAL FC.png') }}" data-src="{{ asset('assets/img/airlines/LOGO SRIWIJAYA AIR HORIZONTAL FC.png') }}" alt=""
+                        <img data-src="{{ asset('assets/img/airlines/qr.png') }}" alt="" class="ic_qr lazy" style="max-width: 30%">
+                        <img data-src="{{ asset('assets/img/airlines/sq.png') }}" alt="" class="ic_sq lazy">
+                        <img data-src="{{ asset('assets/img/airlines/LOGO SRIWIJAYA AIR HORIZONTAL FC.png') }}" alt=""
                             class="ic_sq lazy">
-                        <img src="{{ asset('assets/img/airlines/LOGO NAM AIR HOTIZONTAL FC.png') }}" data-src="{{ asset('assets/img/airlines/LOGO NAM AIR HOTIZONTAL FC.png') }}" alt="" class="ic_sq lazy">
+                        <img data-src="{{ asset('assets/img/airlines/LOGO NAM AIR HOTIZONTAL FC.png') }}" alt="" class="ic_sq lazy">
                     </div>
                 </div>
                 <div class="partner-list partner-group" style="width: 67%">
                     <div class="bold fz12 partner-name">NTO Partners</div>
                     <div class="partner-logo" style="display: flex;">
                         <a target="_blank" class="img-nto" href="https://www.visitkorea.or.id/">
-                            <img src="{{ asset('assets/img/nto/LOGO KTO-1.png') }}" data-src="{{ asset('assets/img/nto/LOGO KTO-1.png') }}" class="ic_korea lazy" alt="" style="padding: 0">
+                            <img data-src="{{ asset('assets/img/nto/LOGO KTO-1.png') }}" class="ic_korea lazy" alt="" style="padding: 0">
                         </a>
                         <a target="_blank" class="img-nto" href="https://www.visitdubai.com/">
-                            <img src="{{ asset('assets/img/nto/Dubai Brand_Color_RGB (1).png') }}" data-src="{{ asset('assets/img/nto/Dubai Brand_Color_RGB (1).png') }}" class="ic_korea lazy" alt="">
+                            <img data-src="{{ asset('assets/img/nto/Dubai Brand_Color_RGB (1).png') }}" class="ic_korea lazy" alt="">
                         </a>
                         <a target="_blank" class="img-nto" href="https://taiwantourism.id/">
-                            <img src="{{ asset('assets/img/nto/TaiwanLogo THOA_red.jpg') }}" data-src="{{ asset('assets/img/nto/TaiwanLogo THOA_red.jpg') }}" class="ic_korea lazy" alt="">
+                            <img data-src="{{ asset('assets/img/nto/TaiwanLogo THOA_red.jpg') }}" class="ic_korea lazy" alt="">
                         </a>
                         <a target="_blank" class="img-nto" href="http://www.wisatathailand.id/">
-                            <img src="{{ asset('assets/img/nto/thai-nto.png') }}" data-src="{{ asset('assets/img/nto/thai-nto.png') }}" class="ic_korea lazy" alt="">
+                            <img data-src="{{ asset('assets/img/nto/thai-nto.png') }}" class="ic_korea lazy" alt="">
                         </a>
                         <a  class="img-nto" >
-                            <img src="{{ asset('assets/img/nto/IMFITP_Colored (1).png') }}" data-src="{{ asset('assets/img/nto/IMFITP_Colored (1).png') }}" class="ic_korea lazy" alt="">
+                            <img data-src="{{ asset('assets/img/nto/IMFITP_Colored (1).png') }}" class="ic_korea lazy" alt="">
                         </a>
                         <a  class="img-nto">
-                            <img src="{{ asset('assets/img/nto/JNTO_logo.png') }}" data-src="{{ asset('assets/img/nto/JNTO_logo.png') }}" class="ic_korea lazy" alt="">
+                            <img data-src="{{ asset('assets/img/nto/JNTO_logo.png') }}" class="ic_korea lazy" alt="">
                         </a>
                     </div>
                 </div>
@@ -108,10 +108,10 @@
                     <div class="bold fz12 partner-name">Supported by</div>
                     <div class="partner-logo">
                         <a target="_blank" class="support1" href="https://dinaskebudayaan.jakarta.go.id/">
-                            <img src="{{ asset('assets/img/dinas.png') }}" data-src="{{ asset('assets/img/dinas.png') }}" class="lazy" style="max-width: 65%" alt="">
+                            <img data-src="{{ asset('assets/img/dinas.png') }}" class="lazy" style="max-width: 65%" alt="">
                         </a>
                         <a target="_blank" class="support2" href="https://www.jakarta-tourism.go.id/">
-                            <img src="{{ asset('assets/img/enjoy jakarta.png') }}" data-src="{{ asset('assets/img/enjoy jakarta.png') }}" class="lazy" style="max-width: 75%" alt="">
+                            <img data-src="{{ asset('assets/img/enjoy jakarta.png') }}" class="lazy" style="max-width: 75%" alt="">
                         </a>
                     </div>
                 </div>
@@ -119,7 +119,7 @@
                     <div class="bold fz12 partner-name">Venue</div>
                     <div class="partner-logo">
                         <a href="https://pikavenue.com" target="_blank">
-                            <img src="{{ asset('assets/img/pik.jpg') }}" data-src="{{ asset('assets/img/pik.jpg') }}" alt="" class="lazy" style="max-width: 50%"> 
+                            <img data-src="{{ asset('assets/img/pik.jpg') }}" alt="" class="lazy" style="max-width: 50%"> 
                         </a>
                     </div>
                 </div>
@@ -127,8 +127,7 @@
                     <div class="bold fz12 partner-name">Organized by</div>
                     <div class="partner-logo">
                         <a href="https://dexkreasi.com" target="_blank">
-                            <img src="{{ asset('assets/img/Logo DEX.png') }}" 
-                                data-src="{{ asset('assets/img/Logo DEX.png') }}" alt="" class="lazy" style="max-width: 35%"> 
+                            <img data-src="{{ asset('assets/img/Logo DEX.png') }}" alt="" class="lazy" style="max-width: 35%"> 
                         </a>
                     </div>
                 </div>
